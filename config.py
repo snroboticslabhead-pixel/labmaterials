f4cc42c1b917be5ae8d69e5e18ec19aaec8aff8d
snroboticslabhead-pixel <snroboticslabhead@gmail.com>
2025-12-08 02:20:45 +0530

    Modify database URI and pool recycle settings
    Updated SQLAlchemy database URI to use pymysql and adjusted connection pool recycle time.

--- config.py
+++ config.py
@@ -9,10 +9,6 @@ class Config:
     MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD') or 'Krishna@532'
     MYSQL_DB = os.environ.get('MYSQL_DB') or 'labmaterial$default'
     
-    # SQLAlchemy configuration
-    SQLALCHEMY_DATABASE_URI = f"mysql+mysqlconnector://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}"
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{MYSQL_USER}:{MYSQL_PASSWORD}@{MYSQL_HOST}/{MYSQL_DB}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ENGINE_OPTIONS = {
-        'pool_recycle': 280,  # Recycle connections after 280 seconds
-        'pool_pre_ping': True  # Enable connection health checks
-    }
+    SQLALCHEMY_POOL_RECYCLE = 299
