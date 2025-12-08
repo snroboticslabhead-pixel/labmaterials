5997cc23a15dde3bcc0fe831c125d1856d41856c
snroboticslabhead-pixel <snroboticslabhead@gmail.com>
2025-12-08 15:55:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,8 +4,9 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key-change-me")
 
     # MySQL configuration for PythonAnywhere
-    MYSQL_HOST = os.environ.get("MYSQL_HOST") or "labmaterial.mysql.pythonanywhere-services.com"
-    MYSQL_USER = os.environ.get("MYSQL_USER") or "labmaterial"
+    MYSQL_HOST = os.environ.get("MYSQL_HOST") or "componentsinfo.mysql.pythonanywhere-services.com"
+    MYSQL_USER = os.environ.get("MYSQL_USER") or "componentsinfo"
     MYSQL_PASSWORD = os.environ.get("MYSQL_PASSWORD") or "Krishna@532"
-    MYSQL_DB = os.environ.get("MYSQL_DB") or "labmaterial$default"
+    MYSQL_DB = os.environ.get("MYSQL_DB") or "componentsinfo$default"
     MYSQL_PORT = int(os.environ.get("MYSQL_PORT", "3306"))
+
